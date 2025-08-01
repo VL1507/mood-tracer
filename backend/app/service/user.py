4d2cd1a9b093f95b6b66efd07754b21026e980be
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-08-02 00:58:03 +1000

    create create_user

--- backend/app/service/user.py
+++ backend/app/service/user.py
@@ -1,8 +1,13 @@
-# from dataclasses import dataclass
+from dataclasses import dataclass
+from app.repository.user import UserRepository
 
 
-# @dataclass
-# class UserService:
-#     # user_repo
+@dataclass
+class UserService:
+    user_repository: UserRepository
 
-#     async def create_user(self, email: str, password: str): ...
+    async def create_user(self, email: str, password_hash: str):
+        user = await self.user_repository.create_user(
+            email=email, password_hash=password_hash
+        )
+        return user.id
