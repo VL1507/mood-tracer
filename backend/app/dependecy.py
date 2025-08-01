14deb7574beb4fd8b637a1e89d44b2464d2e5385
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-08-02 00:51:38 +1000

    rename import User to user

--- backend/app/dependecy.py
+++ backend/app/dependecy.py
@@ -3,7 +3,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.infrastructure.database.accessor import get_db_session
-from app.repository.User import UserRepository
+from app.repository.user import UserRepository
 
 
 async def get_user_repository(
