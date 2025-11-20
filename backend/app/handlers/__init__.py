d8c2dd696ac404ba78483e1da6df4228a5652594
VL1507 <vova.mashyanov@gmail.com>
2025-11-20 21:57:22 +1000

    add redirect_to_docs handler for dev

--- backend/app/handlers/__init__.py
+++ backend/app/handlers/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from . import auth
 from . import test
 from . import user
+from . import redirect_to_docs
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(test.test_router)
 api_router.include_router(user.router)
+api_router.include_router(redirect_to_docs.router)
